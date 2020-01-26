931cd2bbd0d9f331a13dcf740c8d35b64ddd2a0d
inmanturbo <47095624+inmanturbo@users.noreply.github.com>
2020-01-26 01:01:49 -0500

    Update EnvController.php

--- src/Http/Controllers/EnvController.php
+++ src/Http/Controllers/EnvController.php
@@ -1,16 +1,9 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: Fabian
- * Date: 19.07.16
- * Time: 06:17
- */
+namespace Quickweb\SettingsEditor\Http\Controllers;
 
-namespace Quickweb\DotenvEditor\Http\Controllers;
-
-use Quickweb\DotenvEditor\DotenvEditor as Env;
-use Quickweb\DotenvEditor\Exceptions\DotEnvException;
+use Quickweb\SettingsEditor\DotenvEditor as Env;
+use Quickweb\SettingsEditor\Exceptions\DotEnvException;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 
